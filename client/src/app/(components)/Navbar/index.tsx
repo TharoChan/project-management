414f8bd0ce963e5a3879e2d71bf59cd66d7cb261
Tharo Chan <tharochanrith@gmail.com>
2025-01-21 22:24:59 +0700

    feat: add navbar

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="flex item-center justify-between bg-white px-4 py-3 dark:bg-black">Navbar</div>;
+};
+
+
+export default Navbar;
